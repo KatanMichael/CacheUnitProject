209509f3a0573a371495ac9a6971189b15666244
katanmichael <michaelkatan@gmail.com>
2018-04-11 15:01:59 +0300

    Start Testing

--- src/main/java/com/hit/memory/CacheUnit.java
+++ src/main/java/com/hit/memory/CacheUnit.java
@@ -4,6 +4,8 @@ import com.hit.algorithm.IAlgoCache;
 import com.hit.dao.IDao;
 import com.hit.dm.DataModel;
 
+import java.util.ArrayList;
+
 public class CacheUnit <T> 
 {
 
@@ -21,9 +23,23 @@ public class CacheUnit <T>
 	
 	DataModel<T>[] getDataModels(java.lang.Long[] ids) throws java.lang.ClassNotFoundException, java.io.IOException
 	{
-		//TODO FIX THIS!!
-		
-		return null;
+
+		ArrayList<DataModel<T>> listOfEntitys = new ArrayList<>();
+		DataModel<T> entity;
+
+		for (int i = 0; i <ids.length ; i++)
+		{
+			entity = (DataModel<T>) dao.find(ids[i]);
+			if(entity != null)
+			{
+				listOfEntitys.add(entity);
+			}
+
+		}
+
+		DataModel<T>[] tempArr = (DataModel<T>[]) listOfEntitys.toArray();
+		return tempArr;
+
 	}
 
 
--- src/main/test/com/hit/memory/CacheUnitTest.java
+++ src/main/test/com/hit/memory/CacheUnitTest.java
@@ -1,19 +1,39 @@
 package com.hit.memory;
 
 import com.hit.algorithm.LRUAlgoCacheImpl;
-import com.hit.dao.IDao;
 import com.hit.dm.DataModel;
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-import java.io.Serializable;
+import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class CacheUnitTest {
 
-class CacheUnitTest {
-
-    @Test
-    void getDataModels() {
+    @Before
+    public void setUp() throws Exception {
     }
 
-   
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getDataModels()
+    {
+        //DaoFileImpl<Integer> daoFile = new DaoFileImpl<>();
+        //DataModel<Integer> dataModel;
+        LRUAlgoCacheImpl<Long,DataModel<Integer>> lru = new LRUAlgoCacheImpl(5);
+        //CacheUnit<Integer> cacheUnit = new CacheUnit(lru,daoFile);
+
+        lru.putElement(new Long(1),new DataModel<>(new Long(1),new Integer(1)));
+        lru.putElement(new Long(2),new DataModel<>(new Long(2),new Integer(2)));
+        lru.putElement(new Long(3),new DataModel<>(new Long(3),new Integer(3)));
+        lru.putElement(new Long(4),new DataModel<>(new Long(4),new Integer(4)));
+        lru.putElement(new Long(5),new DataModel<>(new Long(5),new Integer(5)));
+
+
+
+        assertEquals(true,true);
+    }
 }
