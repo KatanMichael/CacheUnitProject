acf5e38d36f8d1892b39206b65eeeec5d08105e5
katanmichael <michaelkatan@gmail.com>
2018-05-01 14:04:00 +0300

    Going For Gson!

--- src/main/java/com/hit/dao/DaoFileImpl.java
+++ src/main/java/com/hit/dao/DaoFileImpl.java
@@ -1,5 +1,7 @@
 package com.hit.dao;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.hit.dm.DataModel;
 
 import java.io.*;
@@ -10,65 +12,28 @@ public class DaoFileImpl <T> implements IDao<java.lang.Long,DataModel<T>>
 {
 
 	private ArrayList<DataModel<T>> listOfEntitys;
-
+	private ObjectOutputStream outputStream;
+	private ObjectInputStream inputStream;
+	private Gson gson;
 	private String fileName;
 
 	public DaoFileImpl()
 	{
 		listOfEntitys = new ArrayList<>();
 		fileName = "out.txt";
+		gson = new GsonBuilder().create();
 	}
 
 	public DaoFileImpl(String fileName)
 	{
 		listOfEntitys = new ArrayList<>();
 		this.fileName = fileName;
-
+		gson = new GsonBuilder().create();
 	}
 
 	@Override
 	public void delete(DataModel<T> entity)
 	{
-
-		listOfEntitys.clear();
-		DataModel<T> tempModel;
-
-		try
-		{
-			ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(fileName));
-
-			boolean control = true;
-
-			while(control)
-			{
-				tempModel = (DataModel<T>) inputStream.readObject();
-				if(tempModel != null)
-				{
-					listOfEntitys.add(tempModel);
-				}else
-				{
-					control = false;
-				}
-
-			}
-
-			listOfEntitys.remove(entity);
-
-			ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(fileName,false));
-
-			for(DataModel t: listOfEntitys)
-			{
-				outputStream.writeObject(t);
-			}
-
-
-
-		} catch (IOException e)
-		{
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
 
 
 	}
@@ -76,62 +41,45 @@ public class DaoFileImpl <T> implements IDao<java.lang.Long,DataModel<T>>
 	@Override
 	public DataModel<T> find(Long id)
 	{
-		DataModel <T> tempEntity;
-		DataModel <T> returnEntity = null;
-		try
-		{
-			ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(fileName));
 
-			boolean control = true;
-			while(control)
-			{
-				DataModel<T> readObject = (DataModel<T>) inputStream.readObject();
-				tempEntity = readObject;
-				if(tempEntity != null)
-				{
-					listOfEntitys.add(tempEntity);
-				}else
-				{
-					control = false;
-				}
-
-			}
-
-		} catch (IOException e)
-		{
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		for(DataModel t: listOfEntitys)
-		{
-			if(t.getId() == id)
-			{
-				returnEntity = t;
-			}
-		}
-
-
-		return returnEntity;
 	}
 
 	@Override
 	public void save(DataModel<T> entity)
 	{
+		openStreams();
 
 		try {
-			ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(fileName,true));
-
-			outputStream.writeObject(entity);
-
 			outputStream.close();
-
-		} catch (IOException e)
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+		String jsonString = gson.toJson(entity);
+
+		try {
+			outputStream.writeObject(jsonString);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			outputStream.close();
+			inputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void openStreams()
+	{
+		try {
+			outputStream = new ObjectOutputStream(new FileOutputStream(fileName,true));
+			inputStream = new ObjectInputStream(new FileInputStream(fileName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 
--- src/main/test/com/hit/memory/CacheUnitTest.java
+++ src/main/test/com/hit/memory/CacheUnitTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.UUID;
 
 import static org.junit.Assert.*;
 
@@ -26,41 +27,6 @@ public class CacheUnitTest
     @Test
     public void getDataModels()
     {
-        DataModel<Integer> dataModel;
-        LRUAlgoCacheImpl<Long, DataModel<Integer>> lru = new LRUAlgoCacheImpl<>(25);
-        DaoFileImpl<Integer> daoFile = new DaoFileImpl<>("out.txt");
-
-        for (int i = 0; i < 10; i++)
-        {
-            int integer = i;
-            //daoFile.save(new DataModel(Long.valueOf(i), integer));
-        }
-
-
-        for (int i = 0; i < 5; i++) {
-            lru.putElement((long) i, new DataModel((long) i, i));
-        }
-
-        CacheUnit<Integer> cacheUnit = new CacheUnit(lru, daoFile);
-
-
-
-        Long[] ids = {Long.valueOf(1),Long.valueOf(2),Long.valueOf(15)};
-
-        try {
-            DataModel<Integer>[] dataModels = cacheUnit.getDataModels(ids);
-
-            for(DataModel t: dataModels)
-            {
-                System.out.println(t.toString());
-            }
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
 
 
     }
@@ -68,15 +34,6 @@ public class CacheUnitTest
     @Test
     public void justTesting()
     {
-        try
-        {
-            ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("out.txt",true));
-            ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("out.txt"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
 
     }
 }
